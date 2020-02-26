defd7a8ec5108df107ca29a2b8bf60fd3bc9cedc
Oleksandr Skryl <endlesskwazar@gmail.com>
2020-02-26 18:27:12 +0200

    init

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,13 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
@@ -36,8 +36,9 @@ public class ProductController {
 	}
 	
 	@RequestMapping(value = "/products", method = RequestMethod.POST)
-	public String create(@ModelAttribute Product product) {
+	public String create(@ModelAttribute Product product, RedirectAttributes redirectAttributes) {
 		productService.create(product);
+		redirectAttributes.addFlashAttribute("success_message", "Product created!");
 		return "redirect:/products";
 	}
 	
